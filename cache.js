34a3d0d33c307bc0e66d0010635279bce09de91c
François JOLY <francois@gokam.fr>
2017-11-15 16:24:07 +0100

    remove get parameters from cache urls to avoid problems with session ids or ga crossdomain tracking

--- cache.js
+++ cache.js
@@ -6,6 +6,6 @@
     for (var i = 5 ; i < links.length; i++) {
 	   		links[i].addEventListener('click', function(event) {
 	   		event.preventDefault();
-        	document.location.href = 'http://webcache.googleusercontent.com/search?source=hp&q=cache:'+this.href;
+        	document.location.href = 'http://webcache.googleusercontent.com/search?source=hp&q=cache:'+this.href.split('?')[0];
     	});
 	}
