bc94d75db7ef2736b877ab883a4ca6e872a55005
François JOLY <francois@gokam.fr>
2017-11-15 15:19:13 +0100

    replace links inside google cache webpages to google cache webpages
    except the first 4 which are used by Google

--- cache.js
+++ cache.js
@@ -3,12 +3,9 @@
 	links = document.getElementsByTagName('a');
     console.log(links.length);
     
-    for (var i = 0 ; i < links.length; i++) {
+    for (var i = 5 ; i < links.length; i++) {
 	   		links[i].addEventListener('click', function(event) {
-	   		console.log("we ve got a click to "+this.href);
 	   		event.preventDefault();
-        	alert('http://webcache.googleusercontent.com/search?source=hp&q=cache:'+this.href);
         	document.location.href = 'http://webcache.googleusercontent.com/search?source=hp&q=cache:'+this.href;
-       
     	});
 	}
